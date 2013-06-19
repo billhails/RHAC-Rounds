ea87571e4d07cdde532a341012132fe0ed07e16c
Bill Hails <me@billhails.net>
2013-06-19 20:53:57 +0100

    basic scorecard js now working

--- wp-content/plugins/rhac-scorecards/scorecard.js
+++ wp-content/plugins/rhac-scorecards/scorecard.js
@@ -1,32 +1,72 @@
 
 function addUp() {
     var end = 0;
+    var doz_tot = 0;
+    var total_hits = 0;
+    var total_xs = 0;
+    var total_golds = 0;
+    var total_total = 0;
     for (var dozen = 1; dozen < 9; dozen++) {
+        var doz_hits = 0;
+        var doz_xs = 0;
+        var doz_golds = 0;
+        var doz_doz = 0;
         for (var even in [false, true]) {
             ++end;
             var end_total = 0;
-            document.getElementsByName("end-total-" + end)[0].innerText = "0";
+            for (var arrow = 1; arrow < 7; ++arrow) {
+                var element = document.getElementsByName("arrow-" + end + "-" + arrow)[0];
+                var score = Number(element.getAttribute("score"));
+                end_total += score;
+                doz_doz += score;
+                doz_tot += score;
+                total_total += score;
+                if (score > 0) {
+                    ++doz_hits;
+                    ++total_hits;
+                }
+                if (score > 8) {
+                    ++doz_golds;
+                    ++total_golds;
+                }
+                if (element.value == "X") {
+                    ++doz_xs;
+                    ++total_xs;
+                }
+
+            }
+            document.getElementsByName("end-total-" + end)[0].innerText = String(end_total);
         }
+        document.getElementsByName("doz-hits-" + dozen)[0].innerText = String(doz_hits);
+        document.getElementsByName("doz-xs-" + dozen)[0].innerText = String(doz_xs);
+        document.getElementsByName("doz-golds-" + dozen)[0].innerText = String(doz_golds);
+        document.getElementsByName("doz-doz-" + dozen)[0].innerText = String(doz_doz);
+        document.getElementsByName("doz-tot-" + dozen)[0].innerText = String(doz_tot);
     }
+    document.getElementsByName("total-hits")[0].innerText = String(total_hits);
+    document.getElementsByName("total-xs")[0].innerText = String(total_xs);
+    document.getElementsByName("total-golds")[0].innerText = String(total_golds);
+    document.getElementsByName("total-total")[0].innerText = String(total_total);
 }
 
 function setWatcher(end, dozen, even, arrow) {
     var name = "arrow-" + end + '-' + arrow;
     var input = document.getElementsByName(name)[0];
+    input.setAttribute("score", "0");
     var scoreMap = {
-        X: {score: 10, className: "gold", value: "X"},
-        10: {score: 10, className: "gold", value: "10"},
-        9: {score: 9, className: "gold", value: "9"},
-        8: {score: 8, className: "red", value: "8"},
-        7: {score: 7, className: "red", value: "7"},
-        6: {score: 6, className: "blue", value: "6"},
-        5: {score: 5, className: "blue", value: "5"},
-        4: {score: 4, className: "black", value: "4"},
-        3: {score: 3, className: "black", value: "3"},
-        2: {score: 2, className: "white", value: "2"},
-        1: {score: 1, className: "white", value: "1"},
-        0: {score: 0, className: "miss", value: "M"},
-        M: {score: 0, className: "miss", value: "M"},
+        X: {score: "10", className: "gold", value: "X"},
+        10: {score: "10", className: "gold", value: "10"},
+        9: {score: "9", className: "gold", value: "9"},
+        8: {score: "8", className: "red", value: "8"},
+        7: {score: "7", className: "red", value: "7"},
+        6: {score: "6", className: "blue", value: "6"},
+        5: {score: "5", className: "blue", value: "5"},
+        4: {score: "4", className: "black", value: "4"},
+        3: {score: "3", className: "black", value: "3"},
+        2: {score: "2", className: "white", value: "2"},
+        1: {score: "1", className: "white", value: "1"},
+        0: {score: "0", className: "miss", value: "M"},
+        M: {score: "0", className: "miss", value: "M"},
     };
     input.addEventListener( 'blur',
         function () {
@@ -39,7 +79,7 @@ function setWatcher(end, dozen, even, arrow) {
             } else {
                 input.className = "score";
                 input.value = "";
-                input.setAttribute("score", 0);
+                input.setAttribute("score", "0");
             }
             addUp();
         }
