964388f965c671715485ffe867dbf48881d99fdf
Bill Hails <me@billhails.net>
2013-06-27 19:41:39 +0100

    started to make scorecards into a plugin

---
+++ wp-content/plugins/rhac-scorecards/rhac-scorecards.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: RHAC Score Cards
+Description: Edit, save and view archery scorecards.
+Version: 0.1
+Author: Bill Hails
+Author URI: http://www.billhails.net/
+License: GPL
+*/
+
+define('RHAC_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+
