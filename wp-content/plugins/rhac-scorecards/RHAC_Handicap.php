c48a1f93f74128777cf5f6017ff00e0f35257c2f
Bill Hails <me@billhails.net>
2014-04-05 11:33:21 +0100

    working php handicap calculations

--- wp-content/plugins/rhac-scorecards/RHAC_Handicap.php
+++ wp-content/plugins/rhac-scorecards/RHAC_Handicap.php
@@ -14,7 +14,7 @@ class RHAC_HC_Zone {
 
     public function sum ($lower, $upper, $div) {
         $sum = 0.0;
-        while ($lower < $upper) {
+        while ($lower <= $upper) {
             $sum += $this->calc($lower, $div);
             // print "sum = $sum\n";
             ++$lower;
@@ -51,8 +51,8 @@ abstract class RHAC_Handicap {
         $this->conversion = ($units == "metric" ? 1.0 : 0.9144);
         $this->distances = $distances;
         $this->arrow_radius = $arrow_radius;
-        $this->sigma_theta = pow(1.036, $handicap +  12.9) * 0.0005;
-        $this->K = 0.000001429 * pow(1.07, $handicap + 4.3);
+        $this->sigma_theta = pow(1.036, $handicap +  12.9) * 5E-4;
+        $this->K = 1.429E-6 * pow(1.07, $handicap + 4.3);
     }
 
     protected function square($x) {
@@ -85,7 +85,7 @@ abstract class RHAC_Handicap {
     }
 }
 
-class RHAC_Handicap_Imperial extends RHAC_Handicap {
+class RHAC_Handicap_Imperial extends RHAC_Handicap { # tested
     protected function calc($diameter, $range) {
         $sigma_r_squared = $this->sigma_r_squared($range);
         $zone = new RHAC_HC_Zone($diameter, $this->arrow_radius, $sigma_r_squared);
@@ -93,18 +93,15 @@ class RHAC_Handicap_Imperial extends RHAC_Handicap {
     }
 }
 
-class RHAC_Handicap_Metric extends RHAC_Handicap {
+class RHAC_Handicap_Metric extends RHAC_Handicap { # tested
     protected function calc($diameter, $range) {
         $sigma_r_squared = $this->sigma_r_squared($range);
-
-        // print "sigma_r_squared = $sigma_r_squared\n";
-
         $zone = new RHAC_HC_Zone($diameter, $this->arrow_radius, $sigma_r_squared);
         return 10 - $zone->sum(1, 10, 20);
     }
 }
 
-class RHAC_Handicap_MetricInnerTen extends RHAC_Handicap {
+class RHAC_Handicap_MetricInnerTen extends RHAC_Handicap { # tested
     protected function calc($diameter, $range) {
         $sigma_r_squared = $this->sigma_r_squared($range);
         $zone = new RHAC_HC_Zone($diameter, $this->arrow_radius, $sigma_r_squared);
@@ -112,7 +109,7 @@ class RHAC_Handicap_MetricInnerTen extends RHAC_Handicap {
     }
 }
 
-class RHAC_Handicap_Vegas extends RHAC_Handicap {
+class RHAC_Handicap_Vegas extends RHAC_Handicap { # tested
     protected function calc($diameter, $range) {
         $sigma_r_squared = $this->sigma_r_squared($range);
         $zone = new RHAC_HC_Zone($diameter, $this->arrow_radius, $sigma_r_squared);
@@ -120,7 +117,7 @@ class RHAC_Handicap_Vegas extends RHAC_Handicap {
     }
 }
 
-class RHAC_Handicap_VegasInnerTen extends RHAC_Handicap {
+class RHAC_Handicap_VegasInnerTen extends RHAC_Handicap { # tested
     protected function calc($diameter, $range) {
         $sigma_r_squared = $this->sigma_r_squared($range);
         $zone = new RHAC_HC_Zone($diameter, $this->arrow_radius, $sigma_r_squared);
@@ -128,7 +125,7 @@ class RHAC_Handicap_VegasInnerTen extends RHAC_Handicap {
     }
 }
 
-class RHAC_Handicap_Worcester extends RHAC_Handicap {
+class RHAC_Handicap_Worcester extends RHAC_Handicap { # tested
     protected function calc($diameter, $range) {
         $sigma_r_squared = $this->sigma_r_squared($range);
         $zone = new RHAC_HC_Zone($diameter, $this->arrow_radius, $sigma_r_squared);
--- wp-content/plugins/rhac-scorecards/test.php
+++ wp-content/plugins/rhac-scorecards/test.php
@@ -1,11 +1,168 @@
 <?php
 require_once 'RHAC_Handicap.php';
 
-$frostbite = array(
-    array("D" => 80, "R" => 30, "N" => 36)
+$tests = array(
+
+    array(
+        "round" => "national",
+        "distances" => array(
+            array("D" => 122, "R" => 60, "N" => 48),
+            array("D" => 122, "R" => 50, "N" => 24),
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_Imperial",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 2),
+            array( "handicap" => 50, "expected" => 436),
+            array( "handicap" => 0, "expected" => 648),
+        )
+    ),
+
+    array(
+        "round" => "white 252",
+        "distances" => array(
+            array("D" => 122, "R" => 20, "N" => 36)
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_Imperial",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 65),
+            array( "handicap" => 50, "expected" => 315),
+            array( "handicap" => 0, "expected" => 324),
+        )
+    ),
+
+    array(
+        "round" => "gold 252",
+        "distances" => array(
+            array("D" => 122, "R" => 100, "N" => 36)
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_Imperial",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 0),
+            array( "handicap" => 50, "expected" => 91),
+            array( "handicap" => 0, "expected" => 320),
+        )
+    ),
+
+    array(
+        "round" => "frostbite",
+        "distances" => array(
+            array("D" => 80, "R" => 30, "N" => 36)
+        ),
+        "units" => "metric",
+        "class" => "RHAC_Handicap_Metric",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 5),
+            array( "handicap" => 50, "expected" => 265),
+            array( "handicap" => 0, "expected" => 359),
+        )
+    ),
+
+    array(
+        "round" => "metric i",
+        "distances" => array(
+            array("D" => 122, "R" => 70, "N" => 36),
+            array("D" => 122, "R" => 60, "N" => 36),
+            array("D" => 80, "R" => 50, "N" => 36),
+            array("D" => 80, "R" => 30, "N" => 36),
+        ),
+        "units" => "metric",
+        "class" => "RHAC_Handicap_Metric",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 7),
+            array( "handicap" => 50, "expected" => 817),
+            array( "handicap" => 0, "expected" => 1412),
+        )
+    ),
+
+    array(
+        "round" => "worcester",
+        "distances" => array(
+            array("D" => 40.64, "R" => 20, "N" => 60),
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_Worcester",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 9),
+            array( "handicap" => 50, "expected" => 222),
+            array( "handicap" => 0, "expected" => 300),
+        )
+    ),
+
+    array(
+        "round" => "plymouth",
+        "distances" => array(
+            array("D" => 40, "R" => 15, "N" => 60),
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_Metric",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 39),
+            array( "handicap" => 50, "expected" => 470),
+            array( "handicap" => 0, "expected" => 599),
+        )
+    ),
+
+    array(
+        "round" => "plymouth (triple)",
+        "distances" => array(
+            array("D" => 40, "R" => 15, "N" => 60),
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_Vegas",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 19),
+            array( "handicap" => 50, "expected" => 450),
+            array( "handicap" => 0, "expected" => 599),
+        )
+    ),
+
+    array(
+        "round" => "plymouth inner ten",
+        "distances" => array(
+            array("D" => 40, "R" => 15, "N" => 60),
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_MetricInnerTen",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 39),
+            array( "handicap" => 50, "expected" => 465),
+            array( "handicap" => 0, "expected" => 588),
+        )
+    ),
+
+    array(
+        "round" => "plymouth (triple) inner ten",
+        "distances" => array(
+            array("D" => 40, "R" => 15, "N" => 60),
+        ),
+        "units" => "imperial",
+        "class" => "RHAC_Handicap_VegasInnerTen",
+        "tests" => array(
+            array( "handicap" => 100, "expected" => 19),
+            array( "handicap" => 50, "expected" => 445),
+            array( "handicap" => 0, "expected" => 588),
+        )
+    ),
+
 );
 
-for ($handicap = 0; $handicap <= 100; ++$handicap) {
-    $hc_imperial = new RHAC_Handicap_Metric($handicap, "metric", $frostbite, 0.357);
-    print "$handicap: " . $hc_imperial->predict() . "\n";
+function run_one_test($test) {
+    $class = $test["class"];
+    foreach ($test["tests"] as $testcase) {
+        $hc =  new $class($testcase["handicap"], $test["units"], $test["distances"], 0.357);
+        $result = $hc->predict();
+        if ($result == $testcase["expected"]) {
+            print "$test[round] $testcase[handicap] [OK]\n";
+        }
+        else {
+            print "$test[round] $testcase[handicap] [NOK] got $result expected $testcase[expected]\n";
+        }
+    }
+}
+
+foreach ($tests as $test) {
+    run_one_test($test);
 }
