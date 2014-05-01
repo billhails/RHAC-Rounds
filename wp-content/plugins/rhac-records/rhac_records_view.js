1eaaea314df050686fc84f6bb30a1536593966e2
Bill Hails <me@billhails.net>
2014-05-01 22:48:10 +0100

    Tightened up the layout, added last week's scores

--- wp-content/plugins/rhac-records/rhac_records_view.js
+++ wp-content/plugins/rhac-records/rhac_records_view.js
@@ -1,123 +1,11 @@
+
 function rhacRecordsExplorer() {
+    'use strict';
 
-    function persist(prefix) {
-        var cache = localStorage.getItem(prefix);
-        if (cache == null) {
-            cache = {};
-        }
-        else {
-            cache = JSON.parse(cache);
-        }
-
-        function stash() {
-            try {
-                localStorage.setItem(prefix, JSON.stringify(cache));
-            } catch(e) {
-                if (e == QUOTA_EXCEEDED_ERR) {
-                    alert('Quota Exceeded, please delete some old reports first');
-                }
-            }
-        }
-
-        this.set = function(key, value) {
-            cache[key] = value;
-            stash();
-        }
-
-        this.get = function(key) {
-            return cache[key];
-        }
-
-        this.has = function(key) {
-            return cache.hasOwnProperty(key);
-        }
-
-        this.data = function() {
-            return cache;
-        }
-
-        this.remove = function(key) {
-            delete cache[key];
-            stash();
-        }
-    }
-
-    function semi_persist() {
-        var cache = {};
-
-        this.set = function(key, value) {
-            cache[key] = value;
-        }
-
-        this.get = function(key) {
-            return cache[key];
-        }
-
-        this.has = function(key) {
-            return cache.hasOwnProperty(key);
-        }
-
-        this.data = function() {
-            return cache;
-        }
-
-        this.remove = function(key) {
-            delete cache[key];
-        }
-    }
-
-    var version = "rhac-records-v1.0";
-
-    var persistance;
-    if (typeof(Storage) === "undefined") {
-        alert("You seem to have a very old browser that does not support saving reports, please upgrade!");
-        persistance = new semi_persist();
-    }
-    else {
-        persistance = new persist(version);
-    }
+    var version = "rhac-records-v1.0", persistance;
 
     // TODO - no hard-coded dates
     var predefined_reports = {
-        'Outdoor Scores 2014': {
-            '.rhac-re-outdoor': ['Y'],
-            '#rhac-re-include-lapsed': [''],
-            '#rhac-re-archer': '',
-            '#rhac-re-age': '',
-            '#rhac-re-gender': '',
-            '#rhac-re-bow': '',
-            '.rhac-re-single-round': ['Y'],
-            '#rhac-re-round': '',
-            '#rhac-re-seasons': '2014/01/01-2014/12/31',
-            '#rhac-re-current-records': [''],
-            '#rhac-re-old-records': [''],
-            '#rhac-re-medals': [''],
-            '#rhac-re-252': [''],
-            '#rhac-re-personal-bests': [''],
-            '#rhac-re-handicap-improvements': [''],
-            '#rhac-re-new-classifications': [''],
-            '#rhac-re-reassessments': [''],
-        },
-
-        'Indoor Scores 2013-2014': {
-            '.rhac-re-outdoor': ['N'],
-            '#rhac-re-include-lapsed': [''],
-            '#rhac-re-archer': '',
-            '#rhac-re-age': '',
-            '#rhac-re-gender': '',
-            '#rhac-re-bow': '',
-            '.rhac-re-single-round': ['Y'],
-            '#rhac-re-round': '',
-            '#rhac-re-seasons': '2013/06/01-2014/05/31',
-            '#rhac-re-current-records': [''],
-            '#rhac-re-old-records': [''],
-            '#rhac-re-medals': [''],
-            '#rhac-re-252': [''],
-            '#rhac-re-personal-bests': [''],
-            '#rhac-re-handicap-improvements': [''],
-            '#rhac-re-new-classifications': [''],
-            '#rhac-re-reassessments': [''],
-        },
 
         'Personal Bests': {
             '.rhac-re-outdoor': [''],
@@ -136,7 +24,7 @@ function rhacRecordsExplorer() {
             '#rhac-re-personal-bests': ['Y'],
             '#rhac-re-handicap-improvements': [''],
             '#rhac-re-new-classifications': [''],
-            '#rhac-re-reassessments': [''],
+            '#rhac-re-reassessments': ['']
         },
 
         'Club Records': {
@@ -156,7 +44,7 @@ function rhacRecordsExplorer() {
             '#rhac-re-personal-bests': [''],
             '#rhac-re-handicap-improvements': [''],
             '#rhac-re-new-classifications': [''],
-            '#rhac-re-reassessments': [''],
+            '#rhac-re-reassessments': ['']
         },
 
         '252 Awards': {
@@ -176,7 +64,7 @@ function rhacRecordsExplorer() {
             '#rhac-re-personal-bests': [''],
             '#rhac-re-handicap-improvements': [''],
             '#rhac-re-new-classifications': [''],
-            '#rhac-re-reassessments': [''],
+            '#rhac-re-reassessments': ['']
         },
 
         'All Outdoor Handicaps and Classifications': {
@@ -196,7 +84,7 @@ function rhacRecordsExplorer() {
             '#rhac-re-personal-bests': [''],
             '#rhac-re-handicap-improvements': ['Y'],
             '#rhac-re-new-classifications': ['Y'],
-            '#rhac-re-reassessments': ['Y'],
+            '#rhac-re-reassessments': ['Y']
         },
 
         'All Indoor Handicaps and Classifications': {
@@ -216,26 +104,200 @@ function rhacRecordsExplorer() {
             '#rhac-re-personal-bests': [''],
             '#rhac-re-handicap-improvements': ['Y'],
             '#rhac-re-new-classifications': ['Y'],
-            '#rhac-re-reassessments': ['Y'],
-        },
+            '#rhac-re-reassessments': ['Y']
+        }
 
     };
 
     var all_reports = {};
 
-    function makeDate(lower, upper) {
-        if (lower == '') {
-            if (upper == '') { return ''; }
-            else { return upper; }
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var today_formatted = format_date(d);
+    var last_week_formatted = format_date(new Date(d.getTime() - (7 * 24 * 60 * 60 * 1000)));
+
+    function format_date(date) {
+        var day = String(date.getDate());
+        var month = String(date.getMonth() + 1);
+        var year = String(date.getFullYear());
+        if (day.length === 1) {
+            day = "0" + day;
         }
-        else {
-            if (upper == '') { return lower; }
-            else {
-                if (lower < upper) { return "[".concat(lower, ",", upper); }
-                else if (lower > upper) { return "[".concat(upper, ",", lower); }
-                else { return lower; }
+        if (month.length === 1) {
+            month = "0" + month;
+        }
+        return year + '/' + month + '/' + day;
+    }
+
+    predefined_reports["Last Week's Scores"] = {
+        '.rhac-re-outdoor': [''],
+        '#rhac-re-include-lapsed': [''],
+        '#rhac-re-archer': '',
+        '#rhac-re-age': '',
+        '#rhac-re-gender': '',
+        '#rhac-re-bow': '',
+        '.rhac-re-single-round': ['Y'],
+        '#rhac-re-round': '',
+        '#rhac-re-lower-date': last_week_formatted,
+        '#rhac-re-upper-date': today_formatted,
+        '#rhac-re-current-records': [''],
+        '#rhac-re-old-records': [''],
+        '#rhac-re-medals': [''],
+        '#rhac-re-252': [''],
+        '#rhac-re-personal-bests': [''],
+        '#rhac-re-handicap-improvements': [''],
+        '#rhac-re-new-classifications': [''],
+        '#rhac-re-reassessments': ['']
+    };
+
+
+    if (month >= 6) {
+        predefined_reports["Indoor Scores " + String(year) + "-" + String(year + 1)] = {
+            '.rhac-re-outdoor': ['N'],
+            '#rhac-re-include-lapsed': [''],
+            '#rhac-re-archer': '',
+            '#rhac-re-age': '',
+            '#rhac-re-gender': '',
+            '#rhac-re-bow': '',
+            '.rhac-re-single-round': ['Y'],
+            '#rhac-re-round': '',
+            '#rhac-re-seasons': String(year) + '/06/01-' + String(year + 1) + '/05/31',
+            '#rhac-re-current-records': [''],
+            '#rhac-re-old-records': [''],
+            '#rhac-re-medals': [''],
+            '#rhac-re-252': [''],
+            '#rhac-re-personal-bests': [''],
+            '#rhac-re-handicap-improvements': [''],
+            '#rhac-re-new-classifications': [''],
+            '#rhac-re-reassessments': ['']
+        };
+    }
+
+    while (year > 2012) {
+        predefined_reports["Outdoor Scores " + String(year)] = {
+            '.rhac-re-outdoor': ['Y'],
+            '#rhac-re-include-lapsed': [''],
+            '#rhac-re-archer': '',
+            '#rhac-re-age': '',
+            '#rhac-re-gender': '',
+            '#rhac-re-bow': '',
+            '.rhac-re-single-round': ['Y'],
+            '#rhac-re-round': '',
+            '#rhac-re-seasons': String(year) + '/01/01-' + String(year) + '/12/31',
+            '#rhac-re-current-records': [''],
+            '#rhac-re-old-records': [''],
+            '#rhac-re-medals': [''],
+            '#rhac-re-252': [''],
+            '#rhac-re-personal-bests': [''],
+            '#rhac-re-handicap-improvements': [''],
+            '#rhac-re-new-classifications': [''],
+            '#rhac-re-reassessments': ['']
+        };
+        predefined_reports["Indoor Scores " + String(year - 1) + "-" + String(year)] = {
+            '.rhac-re-outdoor': ['N'],
+            '#rhac-re-include-lapsed': [''],
+            '#rhac-re-archer': '',
+            '#rhac-re-age': '',
+            '#rhac-re-gender': '',
+            '#rhac-re-bow': '',
+            '.rhac-re-single-round': ['Y'],
+            '#rhac-re-round': '',
+            '#rhac-re-seasons': String(year-1) + '/06/01-' + String(year) + '/05/31',
+            '#rhac-re-current-records': [''],
+            '#rhac-re-old-records': [''],
+            '#rhac-re-medals': [''],
+            '#rhac-re-252': [''],
+            '#rhac-re-personal-bests': [''],
+            '#rhac-re-handicap-improvements': [''],
+            '#rhac-re-new-classifications': [''],
+            '#rhac-re-reassessments': ['']
+        };
+        year--;
+    }
+
+    function Persist(prefix) {
+        var cache = localStorage.getItem(prefix);
+        if (cache === null) {
+            cache = {};
+        } else {
+            cache = JSON.parse(cache);
+        }
+
+        function stash() {
+            try {
+                localStorage.setItem(prefix, JSON.stringify(cache));
+            } catch (e) {
+                if (e === QUOTA_EXCEEDED_ERR) {
+                    alert('Quota Exceeded, please delete some old reports first');
+                }
             }
         }
+
+        this.set = function (key, value) {
+            cache[key] = value;
+            stash();
+        };
+
+        this.get = function (key) {
+            return cache[key];
+        };
+
+        this.has = function (key) {
+            return cache.hasOwnProperty(key);
+        };
+
+        this.data = function () {
+            return cache;
+        };
+
+        this.remove = function (key) {
+            delete cache[key];
+            stash();
+        };
+    }
+
+    function Semi_persist() {
+        var cache = {};
+
+        this.set = function (key, value) {
+            cache[key] = value;
+        };
+
+        this.get = function (key) {
+            return cache[key];
+        };
+
+        this.has = function (key) {
+            return cache.hasOwnProperty(key);
+        };
+
+        this.data = function () {
+            return cache;
+        };
+
+        this.remove = function (key) {
+            delete cache[key];
+        };
+    }
+
+
+    if (Storage === undefined) {
+        alert("You seem to have a very old browser that does not support saving reports, please upgrade!");
+        persistance = new Semi_persist();
+    }
+    else {
+        persistance = new Persist(version);
+    }
+    function makeDate(lower, upper) {
+        if (lower === '') {
+            if (upper === '') { return ''; }
+            return upper;
+        }
+        if (upper === '') { return lower; }
+        if (lower < upper) { return "[".concat(lower, ",", upper); }
+        if (lower > upper) { return "[".concat(upper, ",", lower); }
+        return lower;
     }
 
     function doSearch() {
@@ -276,7 +338,7 @@ function rhacRecordsExplorer() {
                 jQuery('#rhac-re-results').html(results);
                 jQuery('#rhac-re-results-table').dataTable(
                     {
-                        "bJQueryUI": true,
+                        "bJQueryUI": true
                     }
                 );
             }
@@ -303,13 +365,37 @@ function rhacRecordsExplorer() {
             '#rhac-re-personal-bests': [jQuery('#rhac-re-personal-bests:checked').val()],
             '#rhac-re-handicap-improvements': [jQuery('#rhac-re-handicap-improvements:checked').val()],
             '#rhac-re-new-classifications': [jQuery('#rhac-re-new-classifications:checked').val()],
-            '#rhac-re-reassessments': [jQuery('#rhac-re-reassessments:checked').val()],
+            '#rhac-re-reassessments': [jQuery('#rhac-re-reassessments:checked').val()]
         };
+    }
+
+    function populateReportMenu() {
+        var names = [];
+        var values = {};
+        var name;
+        var cache = persistance.data();
+        var html = '';
+        var index;
+        for (name in predefined_reports) {
+            values[name] = predefined_reports[name];
+            names.push(name);
+        }
+        for (name in cache) {
+            values[name] = cache[name];
+            names.push(name);
+        }
+        names.sort();
+        for (index in names) {
+            name = names[index];
+            html = html.concat('<option value="', name, '">', name, '</option>');
+            all_reports[name] = values[name];
+        }
+        jQuery('#rhac-re-report').html(html);
     }
 
     function saveReport() {
         var report_name = jQuery('#rhac-re-report-name').val().trim();
-        if (report_name == "") {
+        if (report_name === "") {
             alert("please enter a report name first");
             return;
         }
@@ -322,7 +408,7 @@ function rhacRecordsExplorer() {
                 return;
             }
         }
-        report_data = getCurrentReportSettings();
+        var report_data = getCurrentReportSettings();
         persistance.set(report_name, report_data);
         populateReportMenu();
         jQuery('#rhac-re-report').val(report_name);
@@ -332,7 +418,7 @@ function rhacRecordsExplorer() {
 
     function deleteReport() {
         var report_name = jQuery('#rhac-re-report-name').val().trim();
-        if (report_name == "") {
+        if (report_name === "") {
             alert("please enter a report name first");
             return;
         }
@@ -355,7 +441,7 @@ function rhacRecordsExplorer() {
 
     function changeDates() {
         var season = jQuery('#rhac-re-seasons').val();
-        if (season != '') {
+        if (season !== '') {
             var season_dates = season.split('-');
             jQuery('#rhac-re-lower-date').val(season_dates[0]);
             jQuery('#rhac-re-upper-date').val(season_dates[1]);
@@ -364,10 +450,10 @@ function rhacRecordsExplorer() {
 
     function changeSeasonList() {
         var outdoor = jQuery('.rhac-re-outdoor:checked').val();
-        if (outdoor == "Y") {
+        if (outdoor === "Y") {
             jQuery('#rhac-re-seasons').html(jQuery('#rhac-re-outdoor-seasons').html());
         }
-        else if (outdoor == "N") {
+        else if (outdoor === "N") {
             jQuery('#rhac-re-seasons').html(jQuery('#rhac-re-indoor-seasons').html());
         }
         else {
@@ -380,16 +466,16 @@ function rhacRecordsExplorer() {
     function changeRoundList() {
         var outdoor = jQuery('.rhac-re-outdoor:checked').val();
         var single_round = jQuery('.rhac-re-single-round:checked').val();
-        if (outdoor == "Y") {
-            if (single_round == "Y") {
+        if (outdoor === "Y") {
+            if (single_round === "Y") {
                 jQuery('#rhac-re-round').html(jQuery('#rhac-re-outdoor-rounds').html());
             }
             else {
                 jQuery('#rhac-re-round').html(jQuery('#rhac-re-outdoor-families').html());
             }
         }
-        else if (outdoor == "N") {
-            if (single_round == "Y") {
+        else if (outdoor === "N") {
+            if (single_round === "Y") {
                 jQuery('#rhac-re-round').html(jQuery('#rhac-re-indoor-rounds').html());
             }
             else {
@@ -397,7 +483,7 @@ function rhacRecordsExplorer() {
             }
         }
         else {
-            if (single_round == "Y") {
+            if (single_round === "Y") {
                 jQuery('#rhac-re-round').html(jQuery('#rhac-re-all-rounds').html());
             }
             else {
@@ -418,33 +504,12 @@ function rhacRecordsExplorer() {
     function setForm() {
         var name = jQuery('#rhac-re-report').val();
         var report = all_reports[name];
-        for (var selector in report) {
+        var selector;
+        for (selector in report) {
             jQuery(selector).val(report[selector]);
             jQuery(selector).change();
         }
         jQuery('#rhac-re-report-name').val(name);
-    }
-
-    function populateReportMenu() {
-        var names = [];
-        var values = {};
-        for (var name in predefined_reports) {
-            values[name] = predefined_reports[name];
-            names.push(name);
-        }
-        var cache = persistance.data();
-        for (name in cache) {
-            values[name] = cache[name];
-            names.push(name);
-        }
-        names.sort();
-        var html = '';
-        for (var index in names) {
-            var name = names[index];
-            html = html.concat('<option value="', name, '">', name, '</option>');
-            all_reports[name] = values[name];
-        }
-        jQuery('#rhac-re-report').html(html);
     }
 
     jQuery('#rhac-re-save-report').click(saveReport);
@@ -459,11 +524,12 @@ function rhacRecordsExplorer() {
     populateReportMenu();
     setForm();
     jQuery('#rhac-re-report').change(setForm);
-    jQuery('#rhac-re-edit-report').click(function () { jQuery('#rhac-re-moreform').toggle(); });
+    jQuery('#rhac-re-edit-report').click(function () { jQuery('#rhac-re-moreform').toggle(0); });
 }
 
 jQuery(
-    function() {
+    function () {
+        "use strict";
         jQuery('#rhac-re-main').tooltip();
         jQuery('.accordion').accordion({ heightStyle: "content", collapsible: true });
         if (jQuery('#rhac-re-results')[0]) {
