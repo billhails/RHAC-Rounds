4afa39bbf83cc7cefaf6c9cfb740bf9108f5180e
Bill Hails <me@billhails.net>
2014-06-14 09:55:25 +0100

    first working version

---
+++ wp-content/plugins/rhac-targets/rhac-targets.js
@@ -0,0 +1,95 @@
+jQuery(function() {
+    // arr.forEach(callback(value, index, arr)[, thisArg])
+    var canvas = jQuery('canvas.targets');
+    if (!canvas[0]) {
+        return;
+    }
+    function drawTarget(x, y, mainRadius, name) {
+        canvas.drawArc({
+            fillStyle: 'white',
+            strokeStyle: 'black',
+            strokeWidth: 1,
+            strokeColor: 'black',
+            shadowColor: '#444',
+            shadowBlur: 5,
+            shadowX: 3,
+            shadowY: 3,
+            x: x,
+            y: y,
+            radius: mainRadius,
+            draggable: true,
+            groups: [name],
+            dragGroups: [name]
+        });
+        ['black', 'blue', 'red', 'yellow'].forEach(function(color, index) {
+            var innerRadius = mainRadius * (4.0 - index) / 5.0;
+            canvas.drawArc({
+                fillStyle: color,
+                strokeStyle: color,
+                strokeWidth: 0,
+                strokeColor: color,
+                x: x,
+                y: y,
+                radius: innerRadius,
+                draggable: true,
+                groups: [name],
+                dragGroups: [name]
+            });
+        });
+        canvas.drawRect({
+            fillStyle: '#aaa',
+            x: x,
+            y: y + mainRadius + 8,
+            width: 100,
+            height: 16,
+            draggable: true,
+            shadowColor: '#444',
+            shadowBlur: 5,
+            shadowX: 3,
+            shadowY: 3,
+            groups: [name],
+            dragGroups: [name]
+        }).drawText({
+            fillStyle: 'black',
+            strokeWidth: 0,
+            x: x,
+            y: y + mainRadius + 8,
+            fontSize: 12,
+            fontFamily: 'Verdana, sans-serif',
+            text: name,
+            draggable: true,
+            groups: [name],
+            dragGroups: [name]
+        });
+    }
+    var centerX = 300;
+    var centerY = 300;
+    var originalRadius = 100;
+    // 122cm in yards
+    var mainRadius = originalRadius;
+    [20, 30, 40, 50, 60, 80, 100].forEach(function(yards, index) {
+        var radius = mainRadius * 20.0 / yards;
+        var X = 110 + index * 70;
+        var Y = 20 + radius;
+        drawTarget(X, Y, radius, '122cm @ ' + yards + 'y');
+    });
+
+    // 122cm in meters
+    var mainRadius = originalRadius * 0.94;
+    [20, 30, 40, 50, 60, 70, 90].forEach(function(meters, index) {
+        var radius = mainRadius * 20.0 / meters;
+        var X = 110 + index * 70;
+        var Y = 330 + radius;
+        drawTarget(X, Y, radius, '122cm @ ' + meters + 'm');
+    });
+
+    // 80cm in meters
+    mainRadius = originalRadius * (80 / 122) * 2 * 0.94;
+    [10, 15, 20, 30, 40, 50 ].forEach(function(meters, index) {
+        var radius = mainRadius * 10.0 / meters;
+        var X = 150 + index * 70;
+        var Y = 590 + radius;
+        drawTarget(X, Y, radius, '80cm @ ' + meters + 'm');
+    });
+
+});
---
+++ wp-content/plugins/rhac-targets/rhac-targets.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: RHAC Target Pictures
+Description: Compare target pictures at different distances.
+Version: 0.1
+Author: Bill Hails
+Author URI: http://www.billhails.net/
+License: GPL
+*/
+
+define('RHAC_TARGETS_DIR', plugin_dir_path(__FILE__));
+define('RHAC_TARGETS_URL', plugin_dir_url(__FILE__));
+define('RHAC_TARGETS_ROOT', preg_replace('/[^\/]+\/$/', '', RHAC_TARGETS_DIR));
+
+include_once RHAC_TARGETS_ROOT . 'rhac-3p-deps/rhac-3p-deps.php';
+
+function rhac_load_targets_deps() {
+    rhac_register_3p_scripts();
+    rhac_register_3p_styles();
+    wp_register_script('rhac_targets',
+                       RHAC_TARGETS_URL . 'rhac-targets.js',
+                       array('jcanvas'));
+    wp_enqueue_script('rhac_targets');
+}
+
+add_action('wp_enqueue_scripts', 'rhac_load_targets_deps');
+
+add_shortcode('target_pictures', 'rhac_target_pictures');
+
+function rhac_target_pictures() {
+    return "<canvas width='600' height='1000' class='targets' style='border: 1px solid black;'></canvas>";
+}
